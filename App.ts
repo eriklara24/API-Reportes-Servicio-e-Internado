8b0600b7b35f548fea22a65ade7e2bb9e2d037d1
eriklara24 <tvemd@hotmail.com>
2021-08-28 11:49:39 -0500

    Puerto de la API cambiado, script de la base de datos renombrado

--- App.ts
+++ App.ts
@@ -1,9 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 import config = require('./configuration.json');
-import database from './src/database/index';
 import APIServer from './src/endpoints/index';
-import Usuario from './src/resources/entities/Usuario';
 
 APIServer.listen(config.port, config.host, () => {
   console.log(`API Reporetes Servicio e Internado corriendo ${config.host}:${config.port}`);
