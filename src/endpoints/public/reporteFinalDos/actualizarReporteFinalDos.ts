f850b6b1bab24150cfe27ddca29d16492661d311
KevinOlm <kevin.olmeda@alumnos.udg.mx>
2021-09-04 00:03:41 -0500

    Endpoint aCtualizar reporte final y verificación de un solo servicio por usuario

---
+++ src/endpoints/public/reporteFinalDos/actualizarReporteFinalDos.ts
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+import database from '../../../database';
+
+export default async function actualizarReporteFinalDos(req: any, res: any) {
+  try {
+    const reporteFinalDosActualizado = await database
+      .almacenamientoReporteFinalDos.actualizarReporteFinalDos({
+        id: req.body.id,
+        idServicio: req.body.idServicio,
+        metaAlcanzada: req.body.metaAlcanzada,
+        metodologiaUtilizada: req.body.metodologiaUtilizada,
+        innovacionAportada: req.body.innovacionAportada,
+        conclusiones: req.body.conclusiones,
+        propuestas: req.body.propuestas,
+        actividadesRealizadas: [],
+        atencionesRealizadas: [],
+      });
+    return res.status(201).send(reporteFinalDosActualizado);
+  } catch (err) {
+    return res.status(404).send(err);
+  }
+}
--- src/endpoints/public/servicio/crearServicio.ts
+++ src/endpoints/public/servicio/crearServicio.ts
@@ -3,20 +3,23 @@ import database from '../../../database';
 
 export default async function crearServicio(req: any, res: any) {
   try {
-    const nuevoServicio = await database.almacenamientoServicioGeneral.crearServicioGeneral({
-      id: 0,
-      idUsuario: req.body.idUsuario,
-      entidadReceptora: req.body.entidadReceptora,
-      receptor: req.body.receptor,
-      programa: req.body.programa,
-      objetivosDelPrograma: req.body.objetivosDelPrograma,
-      fechaInicio: req.body.fechaInicio,
-      fechaFin: req.body.fechaFin,
-      totalDeHoras: req.body.totalDeHoras,
-      horarioHoraInicio: req.body.horarioHoraInicio,
-      horarioHoraFin: req.body.horarioHoraFin,
-    });
-    return res.status(201).send(nuevoServicio);
+    if (!await database.almacenamientoServicioGeneral.obtenerPorIdUsuario(req.body.idUsuario)) {
+      const nuevoServicio = await database.almacenamientoServicioGeneral.crearServicioGeneral({
+        id: 0,
+        idUsuario: req.body.idUsuario,
+        entidadReceptora: req.body.entidadReceptora,
+        receptor: req.body.receptor,
+        programa: req.body.programa,
+        objetivosDelPrograma: req.body.objetivosDelPrograma,
+        fechaInicio: req.body.fechaInicio,
+        fechaFin: req.body.fechaFin,
+        totalDeHoras: req.body.totalDeHoras,
+        horarioHoraInicio: req.body.horarioHoraInicio,
+        horarioHoraFin: req.body.horarioHoraFin,
+      });
+      return res.status(201).send(nuevoServicio);
+    }
+    return res.status(400).send({ codigo: 'SERVICIO_YA_EXISTE' });
   } catch (err) {
     return res.status(404).send(err);
   }
